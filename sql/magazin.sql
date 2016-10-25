0b51879447d41cc47585d0bf14ee18ed4170b691
Alexandr <admin@dp-malish.com>
2016-10-25 17:05:30 +0300

    mag sql 0.2

--- sql/magazin.sql
+++ sql/magazin.sql
@@ -47,3 +47,76 @@ CREATE TABLE IF NOT EXISTS mag_ustavnoy_capital(
   )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
   INSERT INTO mag_ustavnoy_capital VALUES('1477344953',3120);
+
+#############################################################################
+
+CREATE TABLE IF NOT EXISTS mag_tovar(
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  id_namenklatura INT,
+  id_nakladnaya INT,
+  zakupki_usd INT NULL,
+  zakupki_nac_val INT NULL,
+  dostavki_usd INT NULL,
+  dostavki_nac_val INT NULL,
+  sebestoimost_usd INT NULL,
+  cena_prodagi INT NULL,
+  data_pokupki INT NULL,
+  data_prodagi INT NULL,
+  PRIMARY KEY(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS mag_namenklatura(
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  razdel VARCHAR(100) NULL,
+  podrazdel VARCHAR(100) NULL,
+  id_nakladnaya INT NULL,
+  shtrihkod INT NULL,
+
+  short_name VARCHAR(255) NULL,
+  full_name MEDIUMTEXT NULL,
+  zakupki_usd INT NULL,
+  zakupki_nac_val INT NULL,
+  skidka INT UNSIGNED NULL,
+
+  brand VARCHAR(120) NULL,
+
+  PRIMARY KEY(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+#####################################################################
+CREATE TABLE IF NOT EXISTS mag_nakladnaya(
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  nomer_nakladnoy VARCHAR(15)NULL,
+  dostavka_usd INT NULL,
+  dostavka_nac_val INT NULL,
+  data INT NOT NULL,
+
+  PRIMARY KEY(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELIMITER //
+CREATE TRIGGER mag_nakladnaya_cena_ins BEFORE INSERT ON mag_nakladnaya
+FOR EACH ROW
+  BEGIN
+    IF(new.dostavka_usd IS NULL)THEN
+      SET @usd=(SELECT cena FROM mag_kurs_valut WHERE valuta="usd");
+      SET new.dostavka_usd=(new.dostavka_nac_val*100)/@usd;
+    END IF;
+  END;//
+
+DELIMITER //
+CREATE TRIGGER mag_nakladnaya_cena_upd BEFORE UPDATE ON mag_nakladnaya
+FOR EACH ROW
+  BEGIN
+    SET @nac_val_old=old.dostavka_nac_val;
+    SET @nac_val_new=new.dostavka_nac_val;
+
+    IF(@nac_val_new IS NOT NULL)THEN
+      IF(@nac_val_old!=@nac_val_new)THEN
+        SET @usd=(SELECT cena FROM mag_kurs_valut WHERE valuta="usd");
+        SET new.dostavka_usd=(new.dostavka_nac_val*100)/@usd;
+      END IF;
+    END IF;
+  END;//
+
+INSERT INTO mag_nakladnaya(id,nomer_nakladnoy,dostavka_usd,dostavka_nac_val,data)VALUES(NULL,'146130',NULL,'26','1477345000');
+#####################################################################
