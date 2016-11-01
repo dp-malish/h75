d97d54d2088d4351d9d338c0a7e54e64ac2496a6
Alexandr <admin@dp-malish.com>
2016-11-01 21:50:27 +0200

    reg  0.3

---
+++ www.mpk.my/index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Документ без названия</title>
+<link rel="stylesheet" type="text/css" href="/css/default.css">
+
+<link rel="stylesheet" type="text/css" href="/css/frame.css">
+<link rel="stylesheet" type="text/css" href="/css/color.css">
+<link rel="stylesheet" type="text/css" href="/css/common.css">
+<link rel="stylesheet" type="text/css" href="/css/temp.css">
+
+</head>
+
+<body>
+
+	<div id="bh" class="maxw"><div class="cl"></div></div>
+    
+    
+    <div id="header" class="maxw">
+    	<header>
+        	
+            <div id="logo" class="fl rel">
+            <a href="/"><img src="/img/site/png.php?img=logo" alt="Логотип" title="На главную"></a>
+            <div class="cl"></div>
+            </div>
+            
+            
+			<div class="h_field">
+
+
+			<div class="cl"></div>
+            </div>
+
+
+			<div class="cl"></div>
+		</header>
+   </div>
+    
+    
+    <div id="ah" class="maxw"><div class="cl"></div></div>
+    
+    <div id="wrap" class="maxw">
+    	<div id="l_col" class="fl rel">
+        <div class="cl"></div>
+        </div>
+
+
+		<div id="m_col" class="rel"><!--Пр кол-->
+        	<div id="r_col" class="fr rel">
+        	<div class="cl"></div>
+            </div><!--Центр кол-->
+            
+            <div id="c_col" class="rel">
+            <main>
+            
+            </main>
+            <div class="cl"></div>
+            </div>
+            
+            
+      <!--Конец Центр кола-->
+      <div class="cl"></div>
+      </div><!--end m_col-->
+    <div class="cl"></div></div><!--end maxw-->
+    
+    
+    <div id="foot" class="rel maxw"><footer>
+    
+    
+    <div id="copy" class="ac">Copyright &copy;<?php echo $site;?> - Детский портал - &laquo;Малыш&raquo; 2015<?php if(date('Y')>2015) echo '-'.date('Y');?><br><br>Использование материалов сайта без разрешения правообладателя запрещено</div>
+    <div id="up"> ^ Наверх </div><div class="cl"></div></footer></div>    
+
+</body>
+</html>
