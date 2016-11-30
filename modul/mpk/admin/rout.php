b48c5adad0a5e9fa7a66775cf1029ac3e9ab705f
Alexandr <admin@dp-malish.com>
2016-12-01 01:14:56 +0200

    mpk admin img upd 0.5

--- modul/mpk/admin/rout.php
+++ modul/mpk/admin/rout.php
@@ -6,7 +6,7 @@ try{if($count_uri_parts>3){throw new Exception();}else{
     require'../modul/'.$dir_site.'/admin/common/img.php';
 
     if(!isset($uri_parts[1])){
-        $main_content='<div class="fon_c">С чего начать?</div>';
+        $main_content='<div class="fon_c"><h3>Настройки</h3></div>';
     }elseif(isset($uri_parts[1])&& !isset($uri_parts[2])){
         switch($uri_parts[1]){
             case'картинки' || 'картинки-изменить':include'../modul/'.$dir_site.'/admin/img/main.php';break;
