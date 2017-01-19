e402b6f3c2612737553bbcd854c477a80117c67b
Alexandr <admin@dp-malish.com>
2017-01-19 15:51:42 +0200

    arminel start 0.01 shablon 0.06 admin 0.0

--- www.arminel.my/index.php
+++ www.arminel.my/index.php
@@ -16,13 +16,15 @@
 
 <script src="/js/jq_3.1.1.js"></script>
 <script src="/js/cufon_1.09i.js"></script>
+<script src="/js/pt_sans.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Marck+Script" rel="stylesheet">
   <!--<script async src="/js/common.php"></script>
   <script async src="/js/slider.php"></script><?/*=$jscript.$css.'<meta name="description" content="'.$description.'"><meta name="keywords" content="'.$keywords.'"><title>'.$title.'</title>';*/?>-->
 </head><body>
 <div class="header rel">
   <div class="headertop rel">
     <div class="maxw">
-      <div class="fl" id="hlm"><h3>Проба пробка</h3></div>
+      <div class="fl" id="hlm"><h3>Саша I live in Mariupol</h3><p>yg yg gygyg k gпраорлтт п рр опр ропо</p></div>
       <div class="fr" id="hrm">
         <ul class="hrm">
           <li><a href="#">1</a></li>
@@ -39,6 +41,6 @@
 
 
 <!--http://templates.cooltemplates.ru/Tsentr-krasoty/index.html  //PageArchiver  http://webkitchen.kiev.ua  -->
-<script type="text/javascript">Cufon.replace('h3', { fontFamily: 'PT Sans', hover:true }); Cufon.now(); </script>
+
 
 </body></html>
