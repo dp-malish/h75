3e2e4dc48b0330ed10f90b49ac9cac093a893062
Alexandr <admin@dp-malish.com>
2017-01-20 01:09:29 +0200

    arminel start 0.02 shablon 0.1 admin 0.0

--- www.arminel.my/index.php
+++ www.arminel.my/index.php
@@ -13,28 +13,41 @@
   <link rel="stylesheet" type="text/css" href="/css/common.css">
   <link rel="stylesheet" type="text/css" href="/css/temp.css">
   <link rel="stylesheet" type="text/css" href="/css/menu.css">
+  <link rel="stylesheet" type="text/css" href="/css/color.css">
 
-<script src="/js/jq_3.1.1.js"></script>
-<script src="/js/cufon_1.09i.js"></script>
-<script src="/js/pt_sans.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Marck+Script" rel="stylesheet">
+<!--<script src="/js/jq_3.1.1.js"></script>-->
   <!--<script async src="/js/common.php"></script>
   <script async src="/js/slider.php"></script><?/*=$jscript.$css.'<meta name="description" content="'.$description.'"><meta name="keywords" content="'.$keywords.'"><title>'.$title.'</title>';*/?>-->
 </head><body>
 <div class="header rel">
   <div class="headertop rel">
     <div class="maxw">
-      <div class="fl" id="hlm"><h3>Саша I live in Mariupol</h3><p>yg yg gygyg k gпраорлтт п рр опр ропо</p></div>
-      <div class="fr" id="hrm">
-        <ul class="hrm">
-          <li><a href="#">1</a></li>
-          <li><a href="#">2</a></li>
-          <li><a href="#">3</a></li>
-        </ul>
+      <div class="fl" id="hlm"></div>
+      <div id="hrm">
+        <div class="dwfe colorhed">
+          <span></span>
+          <span></span>
+          <span><a href="/">Главная</a></span>
+          <span><a href="#">Проект</a></span>
+          <span><a href="/контакты">Контакты</a></span>
+        </div>
       </div>
       <div class="cl"></div>
     </div>
   </div>
+
+  <div class="headercen maxw rel">
+    <div class="colorhed" id="logo">
+      <a href="/">
+      <span>Warmin</span>
+      <span>eL</span>
+      </a>
+    </div>
+    <div class="cl"></div>
+  </div>
+
+  <div class="maxw"><p>kbbkkjwbc Б evrv er gre 123 3 45  6 6 6 7 87 i 9 8998  09 090- 0 123 3 45  6 6 6 7 87 i 9 8998  09 090- 0-   45 45 4  34 43 4343 34 34 2 455 6 7 657 65 7 6   45 45 4  34 43 4343 34 34 2 455 6 7 657 65 7 657 5
+   dw</p></div>
   
 </div>
 
