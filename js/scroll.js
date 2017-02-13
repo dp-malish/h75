f5c8f76d0b5f67fb587351bc2ef9911ec202fdfd
Alexandr <admin@dp-malish.com>
2017-02-13 21:29:06 +0200

    taim start 0.45 shablon 0.91 admin 0.3 main 0.1

---
+++ js/scroll.js
@@ -0,0 +1,15 @@
+window.onscroll=function(){//при скролле показывать и прятать блок
+  if(window.pageYOffset>0&&document.body.clientWidth>800){
+    document.getElementById('up').style.display='block';
+  }else{
+    document.getElementById('up').style.display='none';
+  }
+};
+function scrollUpStart(i){
+  if(i<3)window.scrollTo(0,0);
+  else window.scrollTo(0,i)
+}
+document.getElementById('up').onclick=function(){
+  var i,y=43;
+  for(i=window.pageYOffset;i>0;i--){setTimeout('scrollUpStart('+i+')',y++);}
+};
