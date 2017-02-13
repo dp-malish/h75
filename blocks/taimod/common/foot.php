ed37a1456e34eda6ee0296c1d015f10ca2650491
Alexandr <admin@dp-malish.com>
2017-02-13 14:36:26 +0200

    img ext load 1.0

--- blocks/taimod/common/foot.php
+++ blocks/taimod/common/foot.php
@@ -9,4 +9,49 @@
   </div>
   <div id="bf"><div id="up"> ^ Наверх</div></div>
 </footer>
+<script type="application/javascript">
+
+
+    var scrollUp = document.getElementById('up'); // найти элемент
+
+    scrollUp.onmouseover = function() { // добавить прозрачность
+      scrollUp.style.opacity=0.6;
+      scrollUp.style.filter  = 'alpha(opacity=60)';
+    };
+
+    scrollUp.onmouseout = function() { //убрать прозрачность
+      scrollUp.style.opacity = 0.9;
+      scrollUp.style.filter  = 'alpha(opacity=90)';
+    };
+
+    scrollUp.onclick = function() { //обработка клика
+      window.scrollTo(0,110);
+    };
+
+// show button
+
+    window.onscroll = function () { // при скролле показывать и прятать блок
+      if ( window.pageYOffset > 0 ) {
+        scrollUp.style.display = 'block';
+      } else {
+        scrollUp.style.display = 'none';
+      }
+    };
+    function show_element(res){
+      var op = (temp_obj.style.opacity)?parseFloat(temp_obj.style.opacity):parseInt(temp_obj.style.filter)/100;
+      if(op < res){
+        op += 0.01;
+        temp_obj.style.opacity = op;
+        temp_obj.style.filter='alpha(opacity='+op*100+')';
+        setTimeout('show_element('+res+')',30);
+      }
+    }
+    function start_show(res,obj,res_s){
+      if(res_s === undefined){res_s = 0.3;}
+      temp_obj=obj;
+      temp_obj.style.opacity = res_s;
+      show_element(res);
+    }
+</script>
+
 </body></html>
