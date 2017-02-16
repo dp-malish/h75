cfd1d91c93e7a75eeacc3da24e8a77d38a60fd81
Alexandr <admin@dp-malish.com>
2017-02-16 19:42:45 +0200

    taim start 0.45 shablon 0.91 admin 0.3 main 0.1

--- sql/img.sql
+++ sql/img.sql
@@ -232,3 +232,11 @@ CREATE TABLE IF NOT EXISTS everything_interesting_img(
   content longblob NOT NULL,
   PRIMARY KEY(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS moda_beauty_tips_img(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name_file varchar(255) NOT NULL,
+  png tinyint(1) DEFAULT NULL,
+  content longblob NOT NULL,
+  PRIMARY KEY(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
