34544be3afa1c78694363a56ea6696642ce7c944
Alexandr <admin@dp-malish.com>
2017-02-16 16:25:45 +0200

    view 0.3

--- models/admin/form/InsBlog.php
+++ models/admin/form/InsBlog.php
@@ -73,7 +73,7 @@
         <br><br><br><hr><br><br><br>
 
 
-        <p>img_s | рисунок</p><input type="number" name="img_s" id="img_s" min="1" placeholder="Номер рисунка (маленький)" onchange="img_small()" onblur="img_small()">
+        <p>img_s | рисунок</p><input type="number" name="img_s" id="img_s" min="1" placeholder="Номер рисунка (маленький)" onchange="img_small()" onblur="addTextImg()">
         <div id="img_s_view" class="ac"></div>
                 <script type="application/javascript">
                         function img_small(){
@@ -81,6 +81,24 @@
                                 var s=document.getElementById("form").getAttribute("data-img");
                                 s+=i;
                                 img_s_view.innerHTML='<img src="'+s+'">';
+                        }
+                        function addTextImg(){
+                                var editor = CKEDITOR.instances.full_text;
+                                if(editor.mode=='wysiwyg'){
+                                    editor.insertHtml('<img src="/img/lfkulhit/dbpic.php?id=1">');
+                                }
+                                else alert('Вставка доступна в визуальном режиме!!');
+                                /*var i=document.getElementById('img_s').value;
+                                var s=document.getElementById("form").getAttribute("data-img");
+                                s+=i;*/
+
+                                alert('yu');
+                                /*document.getElementById("full_text").value='<img src="/img/lfkulhit/dbpic.php?id=1">';CKEDITOR.replace('full_text');*/
+                                //var editor = CKEDITOR.instances.full_text.insertHtml('<img src="/img/lfkulhit/dbpic.php?id=1">');
+
+                                alert('yu2');
+                                //document.getElementById("full_text").value='<p>ukhui</p>';
+                                //full_text.innerHTML='<p>ukhui</p>';
                         }
                 </script>
         <input type="hidden" name="img_alt_s" id="img_alt_s">
@@ -91,9 +109,8 @@
 
         <p>short_text | короткий текст</p><textarea type="text" name="short_text" id="short_text" rows="8"></textarea>
                 <br><br><br><hr><br><br><br>
-        <p>img | рисунок</p>
-                <!--<input type="number" name="img" id="img" min="1" placeholder="Номер рисунка" onchange="img_big()">-->
-                <input type="number" name="img" id="img" min="1" placeholder="Номер рисунка" onchange="img_big()">
+                <!--<p>img | рисунок</p><input type="number" name="img" id="img" min="1" placeholder="Номер рисунка" onchange="img_big()">-->
+                <input type="hidden" name="img" id="img">
                 <div id="img_big_view" class="ac"></div>
                 <script type="application/javascript">
                         function img_big(){
@@ -122,4 +139,6 @@
                 <p>Дата публикации*</p><input type="date" name="data" required placeholder="Дата публикации" value="#?">
                 <p>Время публикации*</p><input type="time" name="time" required placeholder="Время публикации" value="#?">
 
-<input type="submit" value="Сохранить"></form><p class="note">Примечание: поля, помеченные * звездочкой - обязательны для заполнения</p></div><script type="application/javascript">CKEDITOR.replace('short_text');CKEDITOR.replace('full_text');</script>
+<input type="submit" value="Сохранить"></form><p class="note">Примечание: поля, помеченные * звездочкой - обязательны для заполнения</p></div><script type="application/javascript">CKEDITOR.replace('short_text');CKEDITOR.replace('full_text');
+        //document.getElementById("full_text").value='<img src="/img/lfkulhit/dbpic.php?id=1">';
+</script>
