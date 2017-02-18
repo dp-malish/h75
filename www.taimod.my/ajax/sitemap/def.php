71fbbc48869703615887f7921b72227ae64cc5fa
Alexandr <admin@dp-malish.com>
2017-02-19 00:51:44 +0200

    uiui

--- www.taimod.my/ajax/sitemap/def.php
+++ www.taimod.my/ajax/sitemap/def.php
@@ -6,11 +6,12 @@ $map=new SiteMap();$DB=new SQLi();
 $map->StartCache();
 echo $map->StartSiteMap();
 
-echo $map->StaticFileMap('modul/main.php',null,'monthly','1.0');
+//echo $map->StaticFileMap('modul/main.php',null,'monthly','1.0');
 
 $def_con=$DB->arrSQL('SELECT link,data FROM default_content');
 foreach($def_con as $it=>$v){echo $map->DBUrlMap($v['link'],$v['data'],'monthly');}
 
 echo $map->EndSiteMap();
 $map->StopCache('def.xml');
-Route::location('/def.xml');
+
+if(isset($_GET['view']))Route::location('/def.xml');
