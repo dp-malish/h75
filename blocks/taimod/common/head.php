8f731ad6ad438a6c0cf26d9f0bbce9ab8814f59e
Alexandr <admin@dp-malish.com>
2017-02-20 23:52:43 +0200

    карта сайта mail ya

--- blocks/taimod/common/head.php
+++ blocks/taimod/common/head.php
@@ -1,4 +1,4 @@
 <!doctype html><html lang="ru"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="author" content="Александр Баранов"><link rel="author" href="https://plus.google.com/105678225473161794317"><meta name="copyright" lang="ru" content="Taimod"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="robots" content="index,follow"><link rel="shortcut icon" href="/img/site/ico.png" type="image/png">
-<meta name='wmail-verification' content='f66b117b127b30993a7f595768e4e742'>
+<meta name='wmail-verification' content='f66b117b127b30993a7f595768e4e742'><meta name="yandex-verification" content="df3cca21cd95d907">
 <link rel="stylesheet" type="text/css" href="/css.php?v=5"><script async src="/js/common.php?v=2"></script><script async src="/js/slider.php?v=2"></script><script async src="/js/scroll.php?id=1"></script><?php
   echo $jscript.$css.'<meta name="description" content="'.$description.'"><meta name="keywords" content="'.$keywords.'"><title>'.$title.'</title>';?></head>
