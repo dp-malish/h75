a558315ba8634be89c52da10e34427be8612611d
Alexandr <admin@dp-malish.com>
2017-03-09 23:28:57 +0200

    dp img upd

--- www.dp.my/modul/t/multiki/uri_parts_1_.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined('MAIN_FILE')){exit;}
-$title=$all_caption.' - '.$title;
-$description=$description.' '.$all_caption;
-$keywords=$uri_parts[1].' '.$keywords;
-
-$main_content.='<h2>Мультфильмы онлайн</h2><h3>'.$all_caption.'</h3><br>';
-$main_content.='<p>Раздел в разработке</p>';
-?>
