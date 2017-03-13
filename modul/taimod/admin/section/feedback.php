de272571e2cb64cd96e287dbc5345ff321cfd76c
Alexandr <admin@dp-malish.com>
2017-03-13 16:36:10 +0200

    блог

--- modul/taimod/admin/section/feedback.php
+++ modul/taimod/admin/section/feedback.php
@@ -1,10 +1,27 @@
 <?php
 if(Validator::paternInt($uri_parts[2])){
     $DB=new SQLi();
-    $res=$DB->strSQL('SELECT name,mail,theme,text,data FROM feedback WHERE id='.$DB->realEscapeStr($uri_parts[2]));
+    if(PostRequest::issetPostArr()){
+        if(PostRequest::issetPostKey(['sms'])){
+            $id=Validator::html_cod($_POST['sms']);
+            if(Validator::paternInt($id)){
+                /*if($DB->boolSQL('UPDATE feedback SET readed=1 WHERE id='.$DB->realEscapeStr($id)))
+                    $main_content.='<div class="fon_c five_ b">Сообщение прочитано...</div>';
+                else $main_content.='<div class="fon_c five_ b">Ошибка сообщения!!!</div>';*/
+                $main_content.='<div class="fon_c five_ b">'.(($DB->boolSQL('UPDATE feedback SET readed=1 WHERE id='.$DB->realEscapeStr($id)))?'Сообщение прочитано...':'Ошибка сообщения!!!').'</div>';
+            }
+        }
+    }
+    $res=$DB->strSQL('SELECT readed,name,mail,theme,text,data FROM feedback WHERE id='.$DB->realEscapeStr($uri_parts[2]));
     if($res){
-
-        $main_content.=$res['name'].$res['mail'].$uri_parts[2];
-
+        $main_content.='<div class="fon_c"><div class="five_">Пользователь: '.$res['name'].'</div>'.
+            '<div class="five_">Почта: '.$res['mail'].'</div>'.
+            '<div class="five_">Тема сообщения: '.$res['theme'].'</div>'.
+            '<div class="five_"><span class="b">Сообщение:</span><br>'.$res['text'].'</div>'.
+            '<div class="five_ ar">Дата: '.Data::IntToStrDateTime($res['data']).'</div>';
+        if($res['readed']==''){
+            $main_content.='<form class="form" method="post"><input type="hidden" name="sms" value="'.$uri_parts[2].'"><input type="submit" value="Прочитано"></form>';
+        }
+        $main_content.='</div>';
     }else $main_content.='Нет такого сообщения...';
 }else $main_content.='feedback...';
