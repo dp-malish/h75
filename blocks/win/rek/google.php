9d00123a5eab189e4c52efdc5ad9c7d64f75a368
Alexandr <admin@dp-malish.com>
2017-03-14 12:23:27 +0200

    блог

--- blocks/win/rek/google.php
+++ blocks/win/rek/google.php
@@ -1,4 +1,4 @@
 <?php
-$rek_winteh='<ins class="adsbygoogle"
-     style="display:block" data-ad-client="ca-pub-0781640196587335" data-ad-slot="5338454119" data-ad-format="auto"></ins>
+$rek_winteh='<div class="grek"><ins class="adsbygoogle"
+     style="display:block" data-ad-client="ca-pub-0781640196587335" data-ad-slot="5338454119" data-ad-format="auto"></ins></div>
 <script>(adsbygoogle=window.adsbygoogle ||[]).push({});</script>';
