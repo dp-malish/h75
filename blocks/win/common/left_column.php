3f19a58a89ad9934a795d8c1f46914fdebf1dea2
Alexandr <admin@dp-malish.com>
2017-03-14 15:27:29 +0200

    блог

--- blocks/win/common/left_column.php
+++ blocks/win/common/left_column.php
@@ -1,1 +1,1 @@
-<div id="wrap" class="maxw"><!--l_c--><div id="l_col" class="rel"><?=$left_content_up.$remont.$left_content;?><div class="cl"></div></div><!--End l_c-->
+<div id="wrap" class="maxw"><div id="l_col" class="rel"><?=$left_content_up.$remont.$rek_winteh_l.$left_content?><div class="cl"></div></div>
--- blocks/win/rek/google.php
+++ blocks/win/rek/google.php
@@ -1,2 +1,3 @@
 <?php
 $rek_winteh='<ins class="adsbygoogle" data-ad-client="ca-pub-0781640196587335" data-ad-slot="5338454119" data-ad-format="auto"></ins><script>(adsbygoogle=window.adsbygoogle ||[]).push({});</script>';
+$rek_winteh_l='';
