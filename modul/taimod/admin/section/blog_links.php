e0671f12d5862a32b5b777b0916e5760c8cce3ba
Alexandr <admin@dp-malish.com>
2017-03-25 11:27:11 +0200

    dp mult

--- modul/taimod/admin/section/blog_links.php
+++ modul/taimod/admin/section/blog_links.php
@@ -1,10 +1,10 @@
 <?php
 $DB=new SQLi();
 $main_content.='<div class="fon_c">';
-$res=$DB->arrSQL('SELECT link,caption FROM content ORDER BY id DESC LIMIT 15');
+$res=$DB->arrSQL('SELECT link,caption,data FROM content ORDER BY id DESC LIMIT 35');// class="gt"
 if($res){
     $main_content.='<h3>Статьи блога</h3><ul class="nav_link">';
-    foreach($res as $k=>$v){$main_content.='<li><a href="/'.$set.'/блог/'.$v['link'].'">'.$v['caption'].'</a></li>';}
+    foreach($res as $k=>$v){$main_content.='<li><a '.($v['data']<time()?'':'class="gt" ').'href="/'.$set.'/блог/'.$v['link'].'">'.$v['caption'].'</a></li>';}
     $main_content.='</ul>';
 }else $main_content.='Нет записей...';
 $main_content.='</div>';
