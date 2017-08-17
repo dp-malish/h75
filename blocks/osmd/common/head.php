ef951033b5e1f351cae016ba6264478c91e5dbce
Alexandr <admin@dp-malish.com>
2017-08-17 21:56:08 +0300

    osmd sql

--- blocks/osmd/common/head.php
+++ blocks/osmd/common/head.php
@@ -4,25 +4,28 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="author" content="Александр Баранов">
   <link rel="author" href="https://plus.google.com/105678225473161794317">
-  <meta name="copyright" lang="ru" content="WinTeh">
 
 
+  <meta name="copyright" lang="ru" content="ОСМД Фортуна 119">
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="robots" content="index,follow">
+  <meta name="robots" content="index,follow"/>
+
+
+  <script async src="/js/common.php"></script>
 
   <!--<link rel="shortcut icon" href="/img/site/ico.png" type="image/png">-->
 
-  <script async src="/js/scroll.php" type="text/javascript"></script>
+  <!--<link rel="stylesheet" type="text/css" href="/css.php?v1">-->
 
-  <script async src="/js/common.php?v1"></script>
 
-  <!--<link rel="stylesheet" type="text/css" href="/css.php">-->
+  <link rel="stylesheet" type="text/css" href="/css/default.css">
   <link rel="stylesheet" type="text/css" href="/css/common.css">
   <link rel="stylesheet" type="text/css" href="/css/frame.css">
   <link rel="stylesheet" type="text/css" href="/css/color.css">
   <link rel="stylesheet" type="text/css" href="/css/menu.css">
   <link rel="stylesheet" type="text/css" href="/css/form.css">
+  
 
 
   <?='<meta name="description" content="'.$description.'"><meta name="keywords" content="'.$keywords.'"><title>'.$title.'</title>'?>
--- www.osmd.my/index.php
+++ www.osmd.my/index.php
@@ -36,51 +36,21 @@ if($_SERVER['REQUEST_URI']!='/'){$uri=htmlspecialchars($_SERVER['REQUEST_URI'],E
 //require'../blocks/mpk/menu/l.php';
 //require'../modul/mpk/common/news.php';
 
-/*require'../blocks/mpk/common/head.php';require'../blocks/mpk/common/befor_header.php';require'../blocks/mpk/common/header.php';require'../blocks/mpk/common/after_header.php';require'../blocks/mpk/common/left_column.php';include'../blocks/mpk/common/body_two_ext.php';require'../blocks/mpk/common/foot.php';*/
+require'../blocks/osmd/common/head.php';
+/*
+require'../blocks/mpk/common/befor_header.php';
+require'../blocks/mpk/common/header.php';require'../blocks/mpk/common/after_header.php';require'../blocks/mpk/common/left_column.php';include'../blocks/mpk/common/body_two_ext.php';require'../blocks/mpk/common/foot.php';*/
 
 ?>
 
 
-<!doctype html>
-<html lang="ru">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="author" content="Александр Баранов">
-	<link rel="author" href="https://plus.google.com/105678225473161794317">
-
-
-	<meta name="copyright" lang="ru" content="ОСМД Фортуна 119">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="robots" content="index,follow"/>
-
-
-	<script async src="/js/common.php"></script>
-
-	<!--<link rel="shortcut icon" href="/img/site/ico.png" type="image/png">-->
-
-	<!--<link rel="stylesheet" type="text/css" href="/css.php?v1">-->
-
-
-	<link rel="stylesheet" type="text/css" href="/css/default.css">
-	<link rel="stylesheet" type="text/css" href="/css/common.css">
-	<link rel="stylesheet" type="text/css" href="/css/frame.css">
-	<link rel="stylesheet" type="text/css" href="/css/color.css">
-	<link rel="stylesheet" type="text/css" href="/css/menu.css">
-	<link rel="stylesheet" type="text/css" href="/css/form.css">
-
-
-	<meta name="description" content="">
-	<meta name="keywords" content="">
-	<title>OSMD</title>
-
-	</head>
-
 	<body>
-
 	<div id="bh" class="maxw">
 	<div class="cl"></div>
 	</div>
+
+
+
 	<div id="header" class="maxw b_fon five_ br">
 		<header>
 			<div id="logo" class="fl rel">
@@ -96,8 +66,12 @@ if($_SERVER['REQUEST_URI']!='/'){$uri=htmlspecialchars($_SERVER['REQUEST_URI'],E
 					<div class="ac">
 
 					</div>
-					<div class="fon_c0 br"><p>ООО "<abbr title="Мариупольский профессиональный колледж">МПК</abbr>" - лидер консалтинговой
-							отрасли<br>в регионе </p></div>
+					<div class="ac">
+						<b>Организация</b>
+						<ul>
+						<li><abbr title="Объединение совладельцев многоквартирного дома">ОСМД</abbr> «Фортуна 119»</li><li>пр-т Строителей 119</li>
+						</ul>
+					</div>
 					<div class="ac">
 						<b>Режим работы:</b>
 						<ul>
@@ -117,42 +91,64 @@ if($_SERVER['REQUEST_URI']!='/'){$uri=htmlspecialchars($_SERVER['REQUEST_URI'],E
 		<div class="cl"></div>
 	</div>
 	
-	
+	<!---------------------------------------------------------------------------->
 	<div id="wrap" class="maxw br five_">
 		<div id="l_col" class="rel">
-			<div class="l_menu rel">
-				<div class="l_menu_title">Спецкурсы</div>
+			<div class="menu_flat rel">
+				<div class="menu_title">Квартиры</div>
 				<nav>
-					<ul>
-						<li><a href="/оператор-компьютерного-набора" title="Оператор компьютерного набора">Оператор ПК</a></li>
-						<li><a href="/администратор" title="Администратор">Администратор</a></li>
-						<li><a href="/агент-по-поставкам" title="Агент по поставкам">Агент по поставкам</a></li>
-						<li><a href="/кассир" title="Кассир на предприятии, в учреждении, организации">Кассир</a></li>
-						<li><a href="/кассир-торгового-зала" title="Кассир торгового зала">Кассир торгового зала</a></li>
-						<li><a href="/цветовод" title="Цветовод">Цветовод</a></li>
-					</ul>
+					<div id="menu_flat" class="dwfe">
+
+
+						<?php
+						for ($i=1;$i<81;$i++)
+						echo '<div><a href="#">'.$i.'</a></div>';
+						?>
+
+					</div>
+
+					<div class="cl"></div>
+
 				</nav>
 			</div>
 			<div class="cl"></div>
 		</div>
+		<!---------------------------------------------------------------------------->
 
-
-		<div id="m_col" class="rel temp"><!--Пр кол-->
+		<div id="m_col" class="rel"><!--Пр кол-->
 			<div id="r_col" class="fr rel">
 				<div class="border fon_c br">
-					<div class="caption"><h3>Новости</h3></div>
+					<div class="caption"><h3>Личный кабинет</h3></div>
 					<ul class="nav_link">
-						<li><a href="/новости/набор-выпускников-11-классов-2017"
-													title="Набор выпускников 11 классов 2017 - узнать подробнее..."><p>Набор выпускников 11 классов 2017</p>
-							</a></li>
+						<li><a href="#"><p>Вход в личный кабинет</p></a></li>
 					</ul>
+				</div>
+				<div class="cl"></div>
+				<div class="menu rel">
+					<div class="menu_title">Меню</div>
+					<nav>
+						<ul>
+							<li><a href="/оператор-компьютерного-набора" title="Оператор компьютерного набора">Оператор ПК</a></li>
+							<li><a href="/администратор" title="Администратор">Администратор</a></li>
+							<li><a href="/агент-по-поставкам" title="Агент по поставкам">Агент по поставкам</a></li>
+							<li><a href="/кассир" title="Кассир на предприятии, в учреждении, организации">Кассир</a></li>
+							<li><a href="/кассир-торгового-зала" title="Кассир торгового зала">Кассир торгового зала</a></li>
+							<li><a href="/цветовод" title="Цветовод">Цветовод</a></li>
+						</ul>
+					</nav>
 				</div>
 				<div class="cl"></div>
 			</div>
 			<!--Центр кол-->
-			<div id="c_col" class="rel">
+			<div id="c_col" class="rel fon_c br">
 				<main>
-				!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>
+					!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>
+					!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>
+					!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>
+					!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>
+					!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>
+					!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>
 				</main>
 				<div class="cl"></div>
 			</div><!--Конец Центр кол-->
@@ -165,8 +161,7 @@ if($_SERVER['REQUEST_URI']!='/'){$uri=htmlspecialchars($_SERVER['REQUEST_URI'],E
 	<div id="foot" class="rel maxw b_fon br five_">
 		<footer>
 
-			<div id="copy" class="ac">Copyright &copy;mpkprof.com.ua<br>Мариупольский профессиональный
-				колледж<br>2013-2017<br><br>Использование материалов сайта без разрешения правообладателя запрещено
+			<div id="copy" class="ac">Copyright &copy;site.com.ua<br>Объединение совладельцев многоквартирного дома «Фортуна 119»<br>2017-2017<br><br>Использование материалов сайта без разрешения правообладателя запрещено
 			</div>
 
 			<div id="up"> ^ Наверх</div>
