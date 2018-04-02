713745fe044bfe3b1871efc174545ef702a35fcf
Alexandr <admin@dp-malish.com>
2018-04-02 22:16:23 +0300

    popular_cat v 0.1 harvi google v 0.1 taimod slider v1.1

--- blocks/taimod/common/slider.php
+++ blocks/taimod/common/slider.php
@@ -1,11 +1,18 @@
 <?php
-$slider='<div class="maxw rel">
-            <div id="main_slider" class="rel">
-                <img src="/img/site/main_slider/jpg.php?i=img1" alt="Мода">
+$slider='<div class="maxw rel"><div id="main_slider" class="rel">';
+
+
+switch($uri_parts[0]){
+    case 'мода':
+        $slider.='<img src="/img/site/main_slider/jpg.php?i=moda1" alt="Мода">
                 <img src="/img/site/main_slider/jpg.php?i=img2" alt="Психология">
                 <img src="/img/site/main_slider/jpg.php?i=img3" alt="Лайфхаки">
-                <img src="/img/site/main_slider/jpg.php?i=img4" alt="Личности">
-                <div></div>
-    <span id="btnSlider"><button type="button" value="0">&nbsp;</button><button type="button" value="1">&nbsp;</button><button
-          type="button" value="2">&nbsp;</button><button type="button" value="3">&nbsp;</button></span></div>
-        </div>';
+                <img src="/img/site/main_slider/jpg.php?i=img4" alt="Личности">';
+        break;
+
+    default:$slider.='<img src="/img/site/main_slider/jpg.php?i=img1" alt="Мода">
+                <img src="/img/site/main_slider/jpg.php?i=img2" alt="Психология">
+                <img src="/img/site/main_slider/jpg.php?i=img3" alt="Лайфхаки">
+                <img src="/img/site/main_slider/jpg.php?i=img4" alt="Личности">';
+}
+$slider.='<div></div><span id="btnSlider"><button type="button" value="0">&nbsp;</button><button type="button" value="1">&nbsp;</button><button type="button" value="2">&nbsp;</button><button type="button" value="3">&nbsp;</button></span></div></div>';
