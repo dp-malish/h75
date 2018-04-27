e2814ab5e3bcf0c9b7e0b08a855ffcbc40543e28
Alexandr <admin@dp-malish.com>
2018-04-27 23:54:41 +0300

    popular_cat v 0.1 harvi google v 0.1 taimod slider v1.1

--- www.mpk.my/img/site/dbpic.php
+++ www.mpk.my/img/site/dbpic.php
@@ -2,4 +2,4 @@
 Error_Reporting(E_ALL & ~E_NOTICE);ini_set('display_errors',1);
 set_include_path('../../../lib');spl_autoload_extensions('_class.php');spl_autoload_register();
 if(isset($_GET['id'])){$id=htmlspecialchars($_GET['id'],ENT_QUOTES);
-if(preg_match("/[0-9]+$/",$id))Img::getImg($id);}else Img::badImg();
+  if(preg_match("/[0-9]+$/",$id))Img::getImg($id);}else Img::badImg();
